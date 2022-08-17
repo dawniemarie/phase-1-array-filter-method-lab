4905c70039329002782904b354be4746a9082787
Dawn Olivier <dmarieolivier@gmail.com>
2022-08-16 22:59:30 -0400

    commit tree 4c75bfb0c99260c00a7c8ecfdce1ff93eab504ac

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const result = drivers.filter(driver => driver === string || driver === string.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(driver => driver.startsWith(string))
+    return result
+}
+
+function matchName(drivers, string) {
+    const result = drivers.filter(driver => string === driver.name)
+    return result
+}
+
+// matchName - This function takes an array of driver objects and a string as arguments. Each driver object has two properties: name and hometown. The function should return each element whose name property matches the provided string argument.
+    //>step 1: create function
+    //>step 2: arr and str as arguments
+    //>step 3: return each element whose name property matches string argument
